41cfeab7ca80f17ef3c437b353d2ab47208b660a
Harsh Kumar <121563719+xtharshh@users.noreply.github.com>
2025-01-01 01:28:46 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getUser",
+          "/api/v1/user/getUser",
           {
             withCredentials: true,
           }
