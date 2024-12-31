c1fbbf9b4b5cc660694e201c0dd5f7ec6a85d348
HARSH KUMAR <xt.harshh@gmail.com>
2024-12-31 22:01:00 +0530

    removed the nm

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const port=process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
