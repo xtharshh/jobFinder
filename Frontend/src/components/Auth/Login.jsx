2d7113696099d3602d5a8a428d0c4294a9aac547
Harsh Kumar <121563719+xtharshh@users.noreply.github.com>
2025-01-01 21:16:49 +0530

    Update Login.jsx

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        `${process.env.FRONTEND_URL}/api/v1/user/login`,
         { email, password, role },
         {
           headers: {
