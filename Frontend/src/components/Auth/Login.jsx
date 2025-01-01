feb615034f8af3fc23394323ae0d251d72bf7e57
Harsh Kumar <121563719+xtharshh@users.noreply.github.com>
2025-01-02 00:13:12 +0530

    Update Login.jsx

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        `${process.env.FRONTEND_URL}/api/v1/user/login`,
+        "https://job-finder-silk.vercel.app/api/v1/user/login",
         { email, password, role },
         {
           headers: {
